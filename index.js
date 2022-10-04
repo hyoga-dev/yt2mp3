5082c2d03becdd6f93c21e83bccb82638dcccc79
hyoga-dev <75929898+hyoga-dev@users.noreply.github.com>
2022-10-04 09:35:29 +0700

    full link

--- index.js
+++ index.js
@@ -1,56 +1,57 @@
-const axios = require('axios');
-const express = require('express')
-require('dotenv').config();
-const cors = require('cors')
-var favicon = require('serve-favicon')
-const path = require('path')
+const axios = require("axios");
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+var favicon = require("serve-favicon");
+const path = require("path");
+const ytp = require("youtube-parser");
 
-const app = express()
-const port = process.env.PORT || 3000
+const app = express();
+const port = process.env.PORT || 3000;
 
 // set template engine
-app.use(cors())
-app.use(express.static( "public" ))
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
-
-
+app.use(cors());
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
 
 // app.get('/', (req, res) => {
 //   res.render('index')
 // })
 
-app.post('/test', (req, res) => {
-  const data = req.body.value
-  
+app.post("/api", (req, res) => {
+  const data = req.body.value;
+
   const options = {
-    method: 'GET',
-    url: 'https://youtube-mp36.p.rapidapi.com/dl',
-    params: {id: data},
+    method: "GET",
+    url: "https://youtube-mp36.p.rapidapi.com/dl",
+    params: { id: data },
     headers: {
-      'X-RapidAPI-Key': process.env.API_KEY,
-      'X-RapidAPI-Host':  process.env.API_HOST
-    }
+      "X-RapidAPI-Key": process.env.API_KEY,
+      "X-RapidAPI-Host": process.env.API_HOST,
+    },
   };
-  
-  axios.request(options)
+
+  axios
+    .request(options)
     .then((response) => {
-    console.log(response.data);
-    res.send(response.data)
-  }).catch((error) => {
-    console.error(error);
-  });
+      console.log(response.data);
+      res.send(response.data);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+});
 
-
-})
-
-app.post('/te', (req, res) => {
+app.post("/test", (req, res) => {
   const data = req.body.value
   console.log(data)
+  // console.log("accessed")
   res.send(req.body)
   res.end()
-})
+});
 
-
-app.listen(port, () => console.log(`Example app listening on  http://localhost:${port}`))
+app.listen(port, () =>
+  console.log(`Example app listening on  http://localhost:${port}`)
+);
--- public/main.js
+++ public/main.js
@@ -1,25 +1,32 @@
-const bot = document.getElementById('bottom')
-async function take() {
-  const inputID = document.getElementById('inputID')
-  const datas = {
-    value: inputID.value
-  }
-  const option = {
-    "method" : "post",
-    "headers": {
-      "content-type": "application/json"
-    },
-    
-    "body": JSON.stringify(datas)
-  };
+const bot = document.getElementById("bottom");
+function youtube_parser(url){
+  var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
+  var match = url.match(regExp);
+  return (match&&match[7].length==11)? match[7] : false;
+}
 
-  const res = await fetch('/test', option)
-  const data = await res.json()
-  console.log(data.link)
+async function take() {
+  const inputID = document.getElementById("inputID");
+
+  const datas = {
+    value: youtube_parser(inputID.value),
+  };
+  const option = {
+    method: "post",
+    headers: {
+      "content-type": "application/json",
+    },
+
+    body: JSON.stringify(datas),
+  };
+  // console.log(datas)
+  // fetch("/api", option)
+  const res = await fetch("/api", option);
+  const data = await res.json();
+  console.log(data.link);
   const txt = `
     <a href=${data.link}><button class="btn">DOWNLOAD</button></a>
-  `
-
+  `;
 
   bot.innerHTML = txt;
   inputID.value = "";
@@ -28,3 +35,5 @@ async function take() {
 function des() {
   bot.innerHTML = "";
 }
+
+// * =================================================
